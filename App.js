ec7ef0a750951dd0c7321b765906f108079fee51
Song Cho <deeplearner507@gmail.com>
2019-04-05 00:27:13 +0900

    updated Height and Width

--- App.js
+++ App.js
@@ -51,30 +51,33 @@ export default class HelloWorldApp extends Component {
     };
 
     return (
-      // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      //   <Text>Hello, world!</Text>
-      //   <Image source={pic} style={{width: 193, height: 110}}/>
-      //   <Greeting name='Rexxar' />
-      //   <Greeting name='Jaina' />
-      //   <Greeting name='Valeera' />
-      //   <Blink text='I love to blink' />
-      //   <Blink text='Yes blinking is so great' />
-      //   <Blink text='Why did they ever take this out of HTML' />
-      //   <Blink text='Look at me look at me look at me' />
-      //   <Text style={styles.red}>just red</Text>
-      //   <Text style={styles.bigBlue}>just bigBlue</Text>
-      //   <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      //   <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
       //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
       //   <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
       //   <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       // Try removing the `flex: 1` on the parent View.
       // The parent will not have dimensions, so the children can't expand.
       // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-          <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
-          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 3}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
+
+        </View>
+        <View style={{flex: 2, backgroundColor: 'skyblue', justifyContent: "center", alignItems: "center" }}>
+          <Text>Hello, world!</Text>
+          <Image source={pic} style={{width: 193, height: 110}}/>
+          <Greeting name='Rexxar' />
+          <Greeting name='Jaina' />
+          <Greeting name='Valeera' />
+          <Text style={styles.red}>just red</Text>
+          <Text style={styles.bigBlue}>just bigBlue</Text>
+          <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+          <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+        </View>
+        <View style={{flex: 1, backgroundColor: 'steelblue', justifyContent: "center", alignItems: "center" }}>
+          <Blink text='I love to blink' />
+          <Blink text='Yes blinking is so great' />
+          <Blink text='Why did they ever take this out of HTML' />
+          <Blink text='Look at me look at me look at me' />        
+        </View> 
       </View> 
     );
   }
