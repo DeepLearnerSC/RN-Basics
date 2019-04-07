6755a7b71288d5536be539de76e7f7b8d6566f0a
Song Cho <song@netrue.co.kr>
2019-04-07 14:19:01 +0900

    Handling Text Input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, TextInput, View, Image, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   bigBlue: {
@@ -45,37 +45,47 @@ class Blink extends Component {
   }
 }
 export default class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
 
     return (
-      // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      //   <Text>Hello, world!</Text>
-      //   <Image source={pic} style={{width: 193, height: 110}}/>
-      //   <Greeting name='Rexxar' />
-      //   <Greeting name='Jaina' />
-      //   <Greeting name='Valeera' />
-      //   <Blink text='I love to blink' />
-      //   <Blink text='Yes blinking is so great' />
-      //   <Blink text='Why did they ever take this out of HTML' />
-      //   <Blink text='Look at me look at me look at me' />
-      //   <Text style={styles.red}>just red</Text>
-      //   <Text style={styles.bigBlue}>just bigBlue</Text>
-      //   <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
-      //   <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
-      //   <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      //   <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-      //   <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
-      // Try removing the `flex: 1` on the parent View.
-      // The parent will not have dimensions, so the children can't expand.
-      // What if you add `height: 300` instead of `flex: 1`?
-      <View style={{flex: 1}}>
-          <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
-          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      <View style={{ flex: 1  }}>
+        <View style={{flex: 2, backgroundColor: 'powderblue', justifyContent: "center", alignItems: "center"}}>
+          <Text>Hello, world!</Text>
+          <Image source={pic} style={{width: 193, height: 110}}/>
+
+        </View>
+        <View style={{flex: 1, backgroundColor: 'skyblue', justifyContent: "center", alignItems: "center"}}>
+          <Blink text='I love to blink' />
+          <Blink text='Yes blinking is so great' />
+          <Blink text='Why did they ever take this out of HTML' />
+          <Blink text='Look at me look at me look at me' />
+        </View>
+        <View style={{flex: 3, backgroundColor: 'steelblue', justifyContent: "center", alignItems: "center"}} >
+          <Greeting name='Rexxar' />
+          <Greeting name='Jaina' />
+          <Greeting name='Valeera' />
+          <Text style={styles.red}>just red</Text>
+          <Text style={styles.bigBlue}>just bigBlue</Text>
+          <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+          <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Type here to translate!"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Text style={{padding: 10, fontSize: 42}}>
+            {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
+          </Text>
+        </View>
       </View> 
     );
   }
-}
+} 
